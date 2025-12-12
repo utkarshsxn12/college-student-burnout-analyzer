b1156251f14a588e0dc78be29ab32ff40202a863
utkarshsxn12 <153208857+utkarshsxn12@users.noreply.github.com>
2025-12-13 01:44:15 +0530

    Updated eda_step1.py

--- eda_step1.py
+++ eda_step1.py
@@ -1,18 +1,8 @@
 import pandas as pd
-
-
 df = pd.read_csv("/Users/utkarshsaxena/Desktop/college-student-burnout-analyzer/data/student_burnout_dataset.csv")
-
-
 print(df.head())
-
-
 print("\nShape:", df.shape)
-
-
 print("\nColumns:")
 print(df.columns)
-
-# Basic info
 print("\nInfo:")
 print(df.info())
